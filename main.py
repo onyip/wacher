4a63f9a4e5fc55d8bebf5647dae496f899b02c5e
OGdangID <onyipgtg@gmail.com>
2022-09-21 23:11:07 +0700

    push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
 
 # options.add_argument(
 #     "--user-data-dir=/home/developer/.config/google-chrome/Profile 19/Profile 01")
@@ -13,14 +14,15 @@ options.add_extension(
 
 options.add_argument("--window-size=100,300")
 d = {}
-for x in range(6):
+for x in range(30):
     d["group" + str(x)] = webdriver.Chrome(
         executable_path=r'./chromedriver', options=options)
     d["group" +
         str(x)].get("https://www.youtube.com/results?search_query=ogdangid pubg")
-
+    time.sleep(5)
     video_number = 1
     find = d["group" + str(x)].find_element(
         "xpath", f"(//a[@id='video-title'])[{video_number}]").click()
+    time.sleep(5)
 
-# tt
+print('y')
