dfd822cb038daf20c1ce256cdeeef2cfe2e6ab5f
OGdangID <onyipgtg@gmail.com>
2022-09-24 08:29:24 +0700

    push

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from selenium.webdriver.chrome.options import Options
 from distutils.dir_util import copy_tree
 import time
 import os
+from pyvirtualdisplay import Display
+
+display = Display(visible=0, size=[800, 600])
+display.start()
 
 options = Options()
 options.add_extension(
@@ -16,20 +20,19 @@ options.add_extension(
 options.add_argument("--window-size=100,300")
 d = {}
 for x in range(2):
-    if os.path.isdir(f'/home/developer/.config/google-chrome/Profile {x}') == False:
+    if os.path.isdir(f'./Profile {x}') == False:
         from_directory = './profile'
-        to_directory = f'/home/developer/.config/google-chrome/Profile {x}'
+        to_directory = f'./Profile {x}'
         copy_tree(from_directory, to_directory)
     options.add_argument(
-        f"--user-data-dir=/home/developer/.config/google-chrome/Profile {x}")
+        f"--user-data-dir=./Profile {x}")
     d["group" + str(x)] = webdriver.Chrome(
         executable_path=r'./chromedriver', options=options)
     d["group" +
-        str(x)].get("https://www.youtube.com/results?search_query=ogdangid pubg")
+        str(x)].get("https://www.youtube.com/results?search_query=OGdangID pubg lite")
     time.sleep(5)
-    video_number = 1
     find = d["group" + str(x)].find_element(
-        "xpath", f"(//a[@id='video-title'])[{video_number}]").click()
+        "xpath", f"(//a[@id='video-title'])[1]").click()
     time.sleep(5)
 
 print('y')
---
+++ profile/prifile.py
@@ -0,0 +1,3 @@
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=[800, 600])
+display.stop()
