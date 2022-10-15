a760cfe3d38f687a7dae675b487a8bc97e459a69
OGdangID <onyipgtg@gmail.com>
2022-10-15 13:49:41 +0700

    update enggine

--- search.py
+++ search.py
@@ -24,7 +24,7 @@ options.add_extension(
     './extension/02.crx')
 options.add_extension(
     './extension/03.crx')
-# options.add_argument("--start-maximized")
+options.add_argument("--start-maximized")
 options.add_experimental_option("excludeSwitches", ['disable-automation'])
 d = {}
 cont = 0
